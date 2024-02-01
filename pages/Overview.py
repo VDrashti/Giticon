50d217c750f6aaebb4dd6ac6a234994afeaaa507
VDrashti <142789565+VDrashti@users.noreply.github.com>
2024-02-01 16:57:37 +0530

    Update Overview.py

--- pages/Overview.py
+++ pages/Overview.py
@@ -21,8 +21,24 @@ import Export_Data
 from st_clickable_images import clickable_images
 import base64
 
-
-
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+hide_github_icon = """
+<style>
+#MainMenu {visibility: hidden;}
+</style>
+"""
+st.markdown(hide_github_icon, unsafe_allow_html=True)
 def build_snowpark_session(kwargs) -> Session:
     try:
         res=Session.builder.configs({
